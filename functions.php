e5866ba1005d65977b30a2b7a08896a89699cbbe
Erik Joling <info@ejoweb.nl>
2024-04-25 16:37:50 +0200

    Add blaadjes, improve portolio style and change loading of stylesheets

--- functions.php
+++ functions.php
@@ -12,17 +12,14 @@
  */
 add_action( 'after_setup_theme', function() {
 
-	// Add support for block styles.
-	add_theme_support( 'wp-block-styles' );
-
-	// Enqueue editor styles.
-	add_editor_style( 'style.css' );
-
 	// Enqueue scripts and styles to frontend
 	add_action( 'wp_enqueue_scripts', 'hgd_add_frontend_styles_and_scripts' );
 
 	// Enqueue scripts and styles to the editor
 	add_action( 'enqueue_block_editor_assets', 'hgd_add_editor_styles_and_scripts' );
+
+	// // Add support for block styles.
+	// add_theme_support( 'wp-block-styles' );
 
 	// Add class to body for featured image
 	add_filter('body_class', function($classes) {
@@ -61,19 +58,21 @@ add_action( 'after_setup_theme', function() {
  */
 function hgd_add_frontend_styles_and_scripts() {
 
-	// Register theme stylesheet.
 	$theme_version = wp_get_theme()->get( 'Version' );
 
-	$version_string = is_string( $theme_version ) ? $theme_version : false;
-	wp_register_style(
-		'hgd-style',
+	wp_enqueue_style(
+		'hgd',
 		get_template_directory_uri() . '/style.css',
 		array(),
-		$version_string
+		$theme_version
 	);
 
-	// Enqueue theme stylesheet.
-	wp_enqueue_style( 'hgd-style' );
+	wp_enqueue_style(
+		'hgd-portfolio',
+		get_template_directory_uri() . '/assets/css/hgd-portfolio.css',
+		array(),
+		$theme_version
+	);
 }
 
 
@@ -82,19 +81,17 @@ function hgd_add_frontend_styles_and_scripts() {
  */
 function hgd_add_editor_styles_and_scripts() {
 
-	// Register theme stylesheet.
 	$theme_version = wp_get_theme()->get( 'Version' );
 
-	$version_string = is_string( $theme_version ) ? $theme_version : false;
-	wp_register_style(
-		'hgd-style-for-editor',
-		get_template_directory_uri() . '/style-for-editor.css',
+	wp_enqueue_style(
+		'hgd-editor-style',
+		get_template_directory_uri() . '/assets/css/editor.css',
 		array(),
-		$version_string
+		$theme_version
 	);
 
-	// Enqueue theme stylesheet.
-	wp_enqueue_style( 'hgd-style-for-editor' );
+	// Enqueue editor styles.
+	add_editor_style( ['style.css', 'assets/css/hgd-portfolio.css'] );
 }
 
 
