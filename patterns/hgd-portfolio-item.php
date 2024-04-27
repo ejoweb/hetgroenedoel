a078a947a6d821737bcfb5dcd7933832cc831c49
Erik Joling <info@ejoweb.nl>
2024-04-27 11:31:54 +0200

    Improve portfolio style and add patterns

---
+++ patterns/hgd-portfolio-item.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Portfolio item
+ * Slug: hgd/portfolio-item
+ * Categories: featured, portfolio
+ * Keywords: project
+ * Block Types: core/group
+ */
+?>
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/placeholder.png","dimRatio":0,"isUserOverlayColor":true,"contentPosition":"top left","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover has-custom-content-position is-position-top-left"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/placeholder.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:details {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|40","right":"var:preset|spacing|40"},"blockGap":"var:preset|spacing|30"},"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"backgroundColor":"primary","textColor":"base-2"} -->
+<details class="wp-block-details has-base-2-color has-primary-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--40)"><summary>Bondige uitspraak over het werk!</summary><!-- wp:paragraph -->
+<p>Review in een paar regels</p>
+<!-- /wp:paragraph --></details>
+<!-- /wp:details --></div></div>
+<!-- /wp:cover -->
---
+++ patterns/hgd-portfolio.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Title: Portfolio
+ * Slug: hgd/portfolio
+ * Categories: featured, portfolio
+ * Keywords: project
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Portfolio Grid"},"align":"full","className":"is-style-hgd-portfolio","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group alignfull is-style-hgd-portfolio">
+	
+	<!-- wp:pattern {"slug":"hgd/portfolio-item"} /-->
+	<!-- wp:pattern {"slug":"hgd/portfolio-item"} /-->
+	<!-- wp:pattern {"slug":"hgd/portfolio-item"} /-->
+	<!-- wp:pattern {"slug":"hgd/portfolio-item"} /-->
+	<!-- wp:pattern {"slug":"hgd/portfolio-item"} /-->
+
+</div>
+<!-- /wp:group -->
